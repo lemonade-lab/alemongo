d8c4a54660daef1e8d533ee2da2819cf1e23c987
xjh <nil541203@gmail.com>
2025-06-22 21:14:27 +0800

    重置机器人模板

--- src/apps/api/settings/updateTemplate.go
+++ src/apps/api/settings/updateTemplate.go
@@ -4,20 +4,16 @@ import (
 	"alemongo/src/apps/api/response"
 	"alemongo/src/logic"
 	"alemongo/src/settings"
+	"alemongo/src/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"path"
 )
 
 // 用于替换旧版本的基础机器人模板
 func ResetTemplate(ctx *gin.Context) {
-	originPath := settings.GetBotTemplate()
 	targetPath := settings.GetResourcesPath()
 
-	if err := logic.ResetTemplate(path.Join(originPath, "bin"), path.Join(targetPath, "bin")); err != nil {
-		response.ResponseErrorWithMsg(ctx, http.StatusBadRequest, http.StatusBadRequest, err.Error())
-	}
-	if err := logic.ResetTemplate(path.Join(originPath, "template"), path.Join(targetPath, "template")); err != nil {
+	if err := logic.ResetTemplate(utils.ResourcesFS, targetPath); err != nil {
 		response.ResponseErrorWithMsg(ctx, http.StatusBadRequest, http.StatusBadRequest, err.Error())
 	}
 	response.ResponseSuccess(ctx, nil)
